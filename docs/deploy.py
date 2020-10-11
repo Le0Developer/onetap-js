5555f52ce185d8e96a8f397e184229b6757857da
Leo Developer <leodeveloper@protonmail.com>
2020-10-11 12:20:25 +0200

    update deploy.py
    PEP-8 and added commit shas to gh-pages commit

--- docs/deploy.py
+++ docs/deploy.py
@@ -4,10 +4,13 @@ import pathlib
 import subprocess
 import tempfile
 
+
 def r(*args, check_output=False, **kwargs):
     if check_output:
         return subprocess.check_output(*args, **kwargs).decode('utf-8')
     return subprocess.call(*args, **kwargs)
+
+
 p = pathlib.Path(__file__).parent
 g = os.getenv
 
@@ -30,10 +33,13 @@ with tempfile.TemporaryDirectory() as tmp:
                     'refs/remotes/origin'], check_output=True).splitlines()
         if x.strip() not in ('HEAD', 'gh-pages')
     ]
+    commit_shas = []
 
     for branch in branches:
         print(f'Building for branch: {branch}')
         r(['git', 'checkout', branch])
+        sha = r(['git', 'rev-parse', 'HEAD'], check_output=True).strip()
+        commit_shas.append(f'{branch} at {sha}')
         r(['sphinx-build', '-b', 'html', str(p), str(tmp / 'en' / branch)])
 
     print('Deploying to Github')
@@ -41,7 +47,14 @@ with tempfile.TemporaryDirectory() as tmp:
     r(['git', 'config', '--global', 'user.email', g('GITHUB_ACTOR') + '@users.noreply.github.com'])
 
     r(['git', 'init'], cwd=str(tmp))
-    r(['git', 'remote', 'add', 'deploy', f'https://token:{g("GITHUB_TOKEN")}@github.com/{g("GITHUB_REPOSITORY")}.git'], cwd=str(tmp))
+    r(
+        [
+            'git', 'remote', 'add', 'deploy',
+            f'https://token:{g("GITHUB_TOKEN")}@'
+            f'github.com/{g("GITHUB_REPOSITORY")}.git'
+         ],
+        cwd=str(tmp)
+    )
     r(['git', 'checkout', '-b', 'gh-pages'], cwd=str(tmp))
 
     with open(tmp / '.nojekyll', 'w') as f:
@@ -64,7 +77,12 @@ with tempfile.TemporaryDirectory() as tmp:
 </html>'''.format(g('GITHUB_REPOSITORY').split('/')[1]))
 
     r(['git', 'add', '-A'], cwd=str(tmp))
-    r(['git', 'commit', '-m', f'deployment of commit {g("GITHUB_REF")}'], cwd=str(tmp))
+    r(
+        [
+            'git', 'commit', '-m',
+            f'deployment of latest push to master\n\n{"".join(commit_shas)}'
+        ],
+        cwd=str(tmp)
+    )
 
     r(['git', 'push', 'deploy', 'gh-pages', '--force'], cwd=str(tmp))
-
