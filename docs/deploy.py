c645112369db5e9d146cb9893b6d68f4291b6b1e
Leo Developer <leodeveloper@protonmail.com>
2021-02-18 18:48:02 +0100

    fix wrong path

--- docs/deploy.py
+++ docs/deploy.py
@@ -21,7 +21,7 @@ r(['make', 'html'], cwd='docs')
 
 with tempfile.TemporaryDirectory() as tmp:
     tmp = Path(tmp)
-    shutil.copy('docs/_build/html', tmp)
+    shutil.copy('docs/_build/main/html', tmp)
     r(['git', 'checkout', '--', '.'])  # discard changes/html
     r(['git', 'checkout', 'gh-pages'])
     r(['git', 'pull'])
