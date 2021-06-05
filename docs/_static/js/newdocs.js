6cce3fba2dc22a499fc66330e680d554f00fb402
Le0Developer <Le0Developer@users.noreply.github.com>
2021-06-05 22:55:20 +0000

    deployment of 08e549c102bf640f7724e680b23f41e2fb93131e

--- docs/_static/js/newdocs.js
+++ docs/_static/js/newdocs.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", function() {
     const a = document.createElement("a");
     a.style = "background-color: #fff59c; color: #000000; display: block; margin: -20px -30px 20px -30px; padding: 30px 30px 20px 30px; text-align: center";
-    a.innerText = "The documentation has been moved to https://leodeveloper.pages.dev.";
-    a.href = "https://leodeveloper.pages.dev";
+    a.innerText = "The documentation has been moved to https://leodev.xyz.";
+    a.href = "https://leodev.xyz/csgo/onetap/v3/docs/";
     document.body.insertBefore(a, document.body.firstChild);
 })
